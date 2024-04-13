6f4061e8eb7d36e45c99f9be4aaf586a901a2ec9
abhisek8094 <iasabhi41@gmail.com>
2024-04-13 19:39:05 +0530

    something changes

--- routes/review.js
+++ routes/review.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync");
-const ExpressError = require("../utils/ExpressError");
-const { reviewSchema } = require("../schema");
 const Review = require("../models/review");
 const Listing = require("../models/listing");
-
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-      let errMsg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(400, errMsg);
-    } else {
-      next();
-    }
-  };
+const {validateReview} = require ("../middleware");
 
 //Post Review Route
 router.post(
