a1853d97440f89322760bba38761b2e9aac93318
abhisek8094 <iasabhi41@gmail.com>
2024-04-13 19:39:41 +0530

    middleware updated

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const Listing = require("./models/listing");
+const { listingSchema, reviewSchema } = require("./schema");
+const ExpressError = require("./utils/ExpressError");
+
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
@@ -9,8 +13,40 @@ module.exports.isLoggedIn = (req, res, next) => {
 };
 
 module.exports.saveRedirectUrl = (req, res, next) => {
-  if(req.session.redirectUrl) {
+  if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
-}
+};
+
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You are not the owner of this listing");
+    return res.redirect(`/listings/${id}`);
+  }
+
+  next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+  const { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
